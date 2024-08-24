60c779115c51609062ad818769da41f1c94f9e7c
OmarAhmed03 <omarelhareef2014@gmail.com>
2024-08-24 10:46:27 +0300

    fix random.py

--- myRandom.py
+++ myRandom.py
@@ -1,4 +1,15 @@
 import numpy as np
 
 r  = np.random.random((2,2))
-print(r)
+
+
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+print(add(r,r))
